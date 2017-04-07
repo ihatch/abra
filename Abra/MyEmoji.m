5ef932b449be9b3835d574e871cb83530f868626
ihatch <ianhat@gmail.com>
2017-04-06 22:45:18 -0400

    cleanup

---
+++ Abra/MyEmoji.m
@@ -0,0 +1,13 @@
+//
+//  MyEmoji.m
+//  EmojiFun
+//
+//  Created by Sabatino Masala on 01/10/15.
+//  Copyright © 2015 Sabatino Masala. All rights reserved.
+//
+
+#import "MyEmoji.h"
+
+@implementation MyEmoji
+
+@end
