a35d1618afa9033852eddfa64322941a1be5f567
Ian Hatcher <ianhat@gmail.com>
2024-11-15 22:16:50 -0700

    is there hope?

--- Podfile
+++ Podfile
@@ -1,9 +1,8 @@
 # Uncomment this line to define a global platform for your project
-platform :ios, "8.0"
+platform :ios, "11.0"
 
 target "Abra" do
   pod 'pop', '~> 1.0'
   pod 'PEPhotoCropEditor'
-  pod 'CocoaLumberjack'
+  pod 'CocoaLumberjack/Swift'
 end
-
--- Pods/Headers/Build/PEPhotoCropEditor/UIImage+PECrop.h
+++ Pods/Headers/Build/PEPhotoCropEditor/UIImage+PECrop.h
@@ -1,1 +1,16 @@
-../../../PEPhotoCropEditor/Lib/UIImage+PECrop.h
+//
+//  UIImage+PECrop.h
+//  PhotoCropEditor
+//
+//  Created by Ernesto Rivera on 2013/07/29.
+//  Copyright (c) 2013 kishikawa katsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (PECrop)
+
+- (UIImage *)rotatedImageWithtransform:(CGAffineTransform)rotation
+                         croppedToRect:(CGRect)rect;
+
+@end
